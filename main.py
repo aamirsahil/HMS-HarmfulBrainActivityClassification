41dbcb1df5009d4bccf76157c2250ff57d3e1eb7
Aamir Sahil <notifysahil@gmail.com>
2024-08-21 20:07:24 +0530

    main file

--- main.py
+++ main.py
@@ -1,5 +1,20 @@
-def main():
+def get_data(path):
     pass
+
+def preprocess_data(data):
+    pass
+
+def train(data):
+    pass
+
+def main():
+    path = ''
+    data = get_data(path)
+    data = preprocess_data(data)
+    model = train(data)
+    test_data = get_data(path)
+    output = model.predict(test_data)
+    print(output)
 
 if __name__=="__main__":
     main()
